ec0fe4acf414684a095fadeefb2b27b53a7be977
atom <28826246+xAtomik@users.noreply.github.com>
2025-05-27 15:56:17 +0100

    Add Get Block Tag Info and Set Block Tag nodes

---
+++ src/main/java/de/blazemcworld/fireflow/code/node/impl/world/SetBlockTagNode.java
@@ -0,0 +1,84 @@
+package de.blazemcworld.fireflow.code.node.impl.world;
+
+import de.blazemcworld.fireflow.code.node.Node;
+import de.blazemcworld.fireflow.code.node.SingleGenericNode;
+import de.blazemcworld.fireflow.code.type.*;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.Items;
+import net.minecraft.state.property.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.Optional;
+
+public class SetBlockTagNode<T> extends SingleGenericNode<T> {
+
+    public SetBlockTagNode(WireType<T> type) {
+        super("set_block_tag", type == null ? "Set Block Tag" : "Set " + type.getName() + " Block Tag", "Sets the value of a block's tag", Items.STONECUTTER, type);
+
+        Input<Void> signal = new Input<>("signal", "Signal", SignalType.INSTANCE);
+        Input<Vec3d> position = new Input<>("position", "Position", VectorType.INSTANCE);
+        Input<String> tag = new Input<>("tag", "Tag", StringType.INSTANCE);
+        Input<T> value = new Input<>("value", "Value", type);
+        Output<Void> next = new Output<>("next", "Next", SignalType.INSTANCE);
+
+        signal.onSignal((ctx) -> {
+            Vec3d pos = position.getValue(ctx);
+            String propertyName = tag.getValue(ctx);
+            T propertyValue = value.getValue(ctx);
+            if (pos.x < -512 || pos.x > 511 || pos.z < -512 || pos.z > 511 || pos.y < ctx.evaluator.world.getBottomY() || pos.y > ctx.evaluator.world.getTopYInclusive()) return;
+
+            BlockPos blockPos = BlockPos.ofFloored(pos);
+            BlockState blockState = ctx.evaluator.world.getBlockState(blockPos);
+            for (Property<?> property : blockState.getProperties()) {
+                if (property.getName().equals(propertyName)) {
+                    switch (property) {
+                        case BooleanProperty booleanProperty when type == ConditionType.INSTANCE -> {
+                            boolean booleanValue = (boolean) propertyValue;
+                            ctx.evaluator.world.setBlockState(blockPos, blockState.with(booleanProperty, booleanValue));
+                        }
+                        case IntProperty intProperty when type == NumberType.INSTANCE -> {
+                            int intValue = ((Double)propertyValue).intValue();
+                            if (intProperty.ordinal(intValue) >= 0) {
+                                ctx.evaluator.world.setBlockState(blockPos, blockState.with(intProperty, intValue));
+                            }
+                        }
+                        case EnumProperty<?> enumProperty when type == StringType.INSTANCE -> {
+                            String stringValue = (String) propertyValue;
+                            if (enumProperty.parse(stringValue).isPresent()) {
+                                ctx.evaluator.world.setBlockState(blockPos, withStringValue(blockState, property, stringValue));
+                            }
+                        }
+                        default -> {}
+                    }
+                    break;
+                }
+            }
+            ctx.sendSignal(next);
+        });
+    }
+
+    private <S extends Comparable<S>> BlockState withStringValue(BlockState blockState, Property<S> property, String value) {
+        Optional<S> parsedValue = property.parse(value);
+        if (parsedValue.isPresent()) {
+            return blockState.with(property, parsedValue.get());
+        }
+        return blockState;
+    }
+
+    @Override
+    public Node copy() {
+        return new SetBlockTagNode<>(type);
+    }
+
+    @Override
+    public boolean acceptsType(WireType<?> type, int index) {
+        return type == ConditionType.INSTANCE || type == NumberType.INSTANCE || type == StringType.INSTANCE;
+    }
+
+    @Override
+    public Node copyWithType(WireType<?> type) {
+        return new SetBlockTagNode<>(type);
+    }
+
+}
