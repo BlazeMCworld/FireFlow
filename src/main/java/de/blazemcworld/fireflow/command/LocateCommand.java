5099908f7864813e2f1256f4c2d8837ee43e7b80
AlignedCookie88 <52969840+AlignedCookie88@users.noreply.github.com>
2025-05-27 14:44:12 +0100

    running /locate with no arguments now locates yourself

--- src/main/java/de/blazemcworld/fireflow/command/LocateCommand.java
+++ src/main/java/de/blazemcworld/fireflow/command/LocateCommand.java
@@ -3,6 +3,7 @@ package de.blazemcworld.fireflow.command;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import de.blazemcworld.fireflow.FireFlow;
 import de.blazemcworld.fireflow.space.Space;
 import de.blazemcworld.fireflow.space.SpaceManager;
@@ -22,6 +23,15 @@ public class LocateCommand {
 
     private static void register(CommandDispatcher<ServerCommandSource> cd, String alias) {
         cd.register(CommandManager.literal(alias)
+                .executes(ctx -> {
+                    ServerPlayerEntity target = ctx.getSource().getPlayer();
+                    if (target == null) {
+                        ctx.getSource().sendMessage(Text.literal("You must be a player to locate yourself.").formatted(Formatting.RED));
+                        return Command.SINGLE_SUCCESS;
+                    }
+
+                    return locateAndRespond(target, ctx);
+                })
                 .then(CommandManager.argument("player", StringArgumentType.word())
                         .suggests((ctx, builder) -> {
                             for (ServerPlayerEntity player : FireFlow.server.getPlayerManager().getPlayerList()) {
@@ -36,39 +46,49 @@ public class LocateCommand {
                                 return Command.SINGLE_SUCCESS;
                             }
 
-                            Space space = SpaceManager.getSpaceForPlayer(target);
-                            ModeManager.Mode mode = ModeManager.getFor(target);
-
-                            switch (mode) {
-                                case LOBBY: {
-                                    ctx.getSource().sendMessage(Text.literal(
-                                            target.getGameProfile().getName() + " is currently in the lobby."
-                                    ).formatted(Formatting.GREEN));
-                                    break;
-                                }
-                                case PLAY: {
-                                    ctx.getSource().sendMessage(Text.literal(
-                                            target.getGameProfile().getName() + " is currently playing on space #" + space.info.id
-                                    ).formatted(Formatting.GREEN));
-                                    break;
-                                }
-                                case CODE: {
-                                    ctx.getSource().sendMessage(Text.literal(
-                                            target.getGameProfile().getName() + " is currently coding on space #" + space.info.id
-                                    ).formatted(Formatting.GREEN));
-                                    break;
-                                }
-                                case BUILD: {
-                                    ctx.getSource().sendMessage(Text.literal(
-                                            target.getGameProfile().getName() + " is currently building on space #" + space.info.id
-                                    ).formatted(Formatting.GREEN));
-                                    break;
-                                }
-                            }
-
-                            return Command.SINGLE_SUCCESS;
+                            return locateAndRespond(target, ctx);
                         }))
         );
+    }
+
+    /**
+     * Locates the target player and sends a response to the command source
+     * @param target The player to locate
+     * @param ctx The command context
+     * @return The success code, hardcoded to <code>Command.SINGLE_SUCCESS</code>
+     */
+    private static int locateAndRespond(ServerPlayerEntity target, CommandContext<ServerCommandSource> ctx) {
+        Space space = SpaceManager.getSpaceForPlayer(target);
+        ModeManager.Mode mode = ModeManager.getFor(target);
+
+        switch (mode) {
+            case LOBBY: {
+                ctx.getSource().sendMessage(Text.literal(
+                        target.getGameProfile().getName() + " is currently in the lobby."
+                ).formatted(Formatting.GREEN));
+                break;
+            }
+            case PLAY: {
+                ctx.getSource().sendMessage(Text.literal(
+                        target.getGameProfile().getName() + " is currently playing on space #" + space.info.id
+                ).formatted(Formatting.GREEN));
+                break;
+            }
+            case CODE: {
+                ctx.getSource().sendMessage(Text.literal(
+                        target.getGameProfile().getName() + " is currently coding on space #" + space.info.id
+                ).formatted(Formatting.GREEN));
+                break;
+            }
+            case BUILD: {
+                ctx.getSource().sendMessage(Text.literal(
+                        target.getGameProfile().getName() + " is currently building on space #" + space.info.id
+                ).formatted(Formatting.GREEN));
+                break;
+            }
+        }
+
+        return Command.SINGLE_SUCCESS;
     }
 
 }
