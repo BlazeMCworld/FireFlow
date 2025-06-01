5013132e9cb0bf8dd49e438ee71793b63892cd2c
Ivy Collective <sys@ivycollective.dev>
2025-06-01 17:52:02 -0400

    fix copy/paste error

--- src/main/java/de/blazemcworld/fireflow/code/node/impl/item/IsItemUnbreakableNode.java
+++ src/main/java/de/blazemcworld/fireflow/code/node/impl/item/IsItemUnbreakableNode.java
@@ -10,7 +10,7 @@ import net.minecraft.item.Items;
 public class IsItemUnbreakableNode extends Node {
 
     public IsItemUnbreakableNode() {
-        super("set_item_unbreakable", "Set Item Unbreakable", "Changes whether an item is unbreakable or not.", Items.IRON_HOE);
+        super("is_item_unbreakable", "Is Item Unbreakable", "Checks if an item is unbreakable or not.", Items.IRON_HOE);
 
         Input<ItemStack> item = new Input<>("item", "Item", ItemType.INSTANCE);
         Output<Boolean> unbreakable = new Output<>("unbreakable", "Unbreakable", ConditionType.INSTANCE);
